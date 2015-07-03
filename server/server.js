4688525767b6199d5569a3d5aae1fab50e03db11
Juan Carlos Roca <juancroca@gmail.com>
2015-07-03 18:32:39 +0200

    initial hapijs stuff

---
+++ server/server.js
@@ -0,0 +1,41 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+// Set some server stuff
+server.connection({
+    port: 3000
+});
+
+
+// Serve static files (css and js)
+server.route({
+    method: 'GET',
+    path: '/static/{param*}',
+    handler: {
+        directory: {
+            path: 'public/static'
+        }
+    }
+});
+
+// Load the routes
+var routes = require('./routes');
+// Register the routes to the server
+for (var i = 0; i < routes.length; i++) {
+    console.log('Registered [' + routes[i].method + '] ' + routes[i].path);
+    server.route(routes[i]);
+}
+
+// Load the plugins
+var plugins = require('./config/plugins');
+for(var name in plugins) {
+    console.log('Registered ' + name + " plugin");
+    var plug = plugins[name].options ? {register: plugins[name].register, options: plugins[name].options} : plugins[name];
+    server.register(plug, function (err) {
+        if (err) {
+            console.log('Failed loading ' + name + ' plugin: ' + err.toString());
+        }
+    });
+}
+
+module.exports = server;
